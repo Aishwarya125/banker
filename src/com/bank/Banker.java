b741385df3302c98104f80e9356fc7d5b6a53b1d
Aishwarya125 <aishwaryaharini@gmail.com>
2022-05-23 20:00:27 +0530

    pending

--- src/com/bank/Banker.java
+++ src/com/bank/Banker.java
@@ -1,5 +1,23 @@
 package com.bank;
 
 public class Banker {
+	
 
-}
+	private void savings() {
+		// TODO Auto-generated method stub
+	System.out.println("SAVINGS 6%");
+	}private void fixed() {
+		// TODO Auto-generated method stub
+	System.out.println("FIXED 6%");
+	}private void deposit() {
+		// TODO Auto-generated method stub
+	System.out.println("DEPOSIT 7%");
+	}public static void main(String[] args) {
+		Banker b=new Banker();
+		b.savings();
+		b.fixed();
+		b.deposit();
+	}
+	}
+
+
